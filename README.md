4cc4fcc4542a91584bb1d5c72569758f99ae48a5
AverageJoeWang <wlf602174464@qq.com>
2017-12-17 10:27:23 +0800

    Degree of an Array

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ LeetCode solutions in Java.
 |---|-----|--------|--------|----------|
 |001|[Two Sum][001]|[JAVA](_001_TwoSum.java)|2017/12/15|Easy|
 |088|[Merge Sorted Array][088]|Method1:[JAVA1](_088_MergeSortedArray.java) Method2:[JAVA2](_088_MergeSortedArray_with_NoMoreSpace.java)|2017/12/15|Easy|
+|697|[Degree of an Array][001]|[JAVA](_697_DegreeofanArray.java)|2017/12/17|Easy|
 |717|[1-bit and 2-bit Characters][717]|method1:[JAVA](_717_1bit_and_2bitCharacters.java)method2:[JAVA](_717_1bit_and_2bitCharacters_2.java)|2017/12/16|Easy|
 
 ## tags:Array
@@ -16,6 +17,7 @@ LeetCode solutions in Java.
 |---|-----|--------|--------|----------|
 |001|[Two Sum][001]|[JAVA](_001_TwoSum)|2017/12/15|Easy|
 |088|[Merge Sorted Array][088]|Method1:[JAVA1](_088_MergeSortedArray.java) Method2:[JAVA2](_088_MergeSortedArray_with_NoMoreSpace.java)|2017/12/15|Easy|
+|697|[Degree of an Array][001]|[JAVA](_697_DegreeofanArray.java)|2017/12/17|Easy|
 |717|[1-bit and 2-bit Characters][717]|method1:[JAVA](_717_1bit_and_2bitCharacters.java)method2:[JAVA](_717_1bit_and_2bitCharacters_2.java)|2017/12/16|Easy|
 
 [001]:https://leetcode.com/problems/two-sum/description/
---
+++ _697_DegreeofanArray.java
@@ -0,0 +1,44 @@
+/**
+ * Created by wanglifei on 2017/12/17.
+ */
+public class _697_DegreeofanArray {
+    public int findShortestSubArray(int[] nums) {
+        int count[];
+        count = new int[50000];
+        int maxCount = 1, cnt, minCount = 50001;
+        for (int i = 0; i < nums.length; i++)//hash列表统计每个数字出现的次数，记录出现最大次数(degree)
+        {
+            if (count[nums[i]] == -1)
+                count[nums[i]] = 1;
+            else
+            {
+                count[nums[i]] = count[nums[i]] + 1;
+                if (count[nums[i]] > maxCount)
+                    maxCount = count[nums[i]];
+            }
+        }
+        for (int i = 0; i < count.length; i++)//遍历hash表
+        {
+            cnt = 0;
+            if (maxCount == count[i])//取得最大出现次数，因为可能出现次数相同但是数值不一样的数字
+            {
+                int k = maxCount;
+                for (int j = 0; j < nums.length; j++)//遍历原数组，统计记录最小子列
+                {
+                    if (k == 0)
+                        break;
+                    if (nums[j] == i)
+                    {
+                        k--;
+                        cnt++;
+                    }
+                    if (nums[j] != i && k != maxCount)
+                        cnt++;
+                }
+                if (cnt < minCount)
+                    minCount = cnt;
+            }
+        }
+        return minCount;
+    }
+}

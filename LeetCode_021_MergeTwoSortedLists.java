bd27aa9c446e763870d52348d8dd341e4c722889
AverageJoeWang <wlf602174464@qq.com>
2018-02-04 23:51:07 +0800

    update

--- LeetCode_021_MergeTwoSortedLists.java
+++ LeetCode_021_MergeTwoSortedLists.java
@@ -7,7 +7,6 @@ public class LeetCode_021_MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
-        ListNode node = new ListNode(-1);
         if (l1.val < l2.val)
         {
             ListNode temp = l1;

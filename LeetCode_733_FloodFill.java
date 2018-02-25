b0745c82416b79c0d3ab78e7d1b647afd71a56b0
AverageJoeWang <wlf602174464@qq.com>
2018-02-25 21:59:10 +0800

    update

---
+++ LeetCode_733_FloodFill.java
@@ -0,0 +1,21 @@
+/**
+ * Created by oliverwang on 2018/2/25.
+ */
+
+
+public class LeetCode_733_FloodFill {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) return image;
+        fill(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+
+    private void fill(int[][] image, int sr, int sc, int color, int newColor) {
+        if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) return;
+        image[sr][sc] = newColor;
+        fill(image, sr + 1, sc, color, newColor);
+        fill(image, sr - 1, sc, color, newColor);
+        fill(image, sr, sc + 1, color, newColor);
+        fill(image, sr, sc - 1, color, newColor);
+    }
+}
